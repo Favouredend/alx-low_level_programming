53deabb5615670458b6d50c28258e2370bcd6960
Favouredend <anosikefavour@gmail.com>
2022-06-23 07:03:24 -0700

    all on recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 int _putchar(char c);
 
 void _puts_recursion(char *s);
@@ -14,20 +13,3 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 
 int is_prime_number(int n);
-=======
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-int _putchar(char);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-
-#endif
->>>>>>> 6fe53eda91e4c10f547f4a15e0e45285e2be139c
